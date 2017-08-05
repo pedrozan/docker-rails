2615e476b04aaecf7549999037418cbe99cfebd0
Pedro Henrique <pedroschleder@outlook.com>
2017-08-05 01:16:00 -0300

    changing docker composer location

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose
-chmod +x /opt/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
