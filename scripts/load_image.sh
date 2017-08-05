a189add79286595a38a26449fb7d3dc15127699a
Pedro Henrique <pedroschleder@outlook.com>
2017-08-05 01:47:56 -0300

    adding docker instalation to scripts

--- scripts/load_image.sh
+++ scripts/load_image.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+systemctl status docker
+
 curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
