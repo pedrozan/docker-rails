bfa0a5119e6f262b99333666c046fed970e17a83
Pedro Henrique <pedroschleder@outlook.com>
2017-08-05 01:29:22 -0300

    changing call to docker-composer

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,5 @@
 #!usr/bin/env bash
 
-/usr/bin/docker-compose -f docker-compose.yml build -d > /dev/null 2> /dev/null < /dev/null &
-/usr/bin/docker-compose -f docker-compose.yml up -d > /dev/null 2> /dev/null < /dev/null &
-/usr/bin/docker-compose -f docker-compose.yml run web rake db:create -d > /dev/null 2> /dev/null < /dev/null &
+docker-compose build -d > /dev/null 2> /dev/null < /dev/null &
+docker-compose up -d > /dev/null 2> /dev/null < /dev/null &
+docker-compose run web rake db:create -d > /dev/null 2> /dev/null < /dev/null &
